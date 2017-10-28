d0a0c4411c02aa3a1670f00690500cdef778bc23
fnadalt <fnt2006@gmail.com>
2017-10-28 10:40:32 -0300

    rclone_sync

--- rclone_sync_remote.sh
+++ rclone_sync_remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Actualizar GDrive"
-rclone sync -u $1 . GDrive:/panda3d/mundo --filter-from rclone-filter.txt --verbose=1
+rclone sync $1 . GDrive:/panda3d/mundo --filter-from rclone-filter.txt --verbose=1
