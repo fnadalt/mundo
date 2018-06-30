daa6d4f0f2198208cc68c6d1cde549ce1bb83017
flacox <fnt2006@gmail.com>
2018-06-30 09:28:34 -0300

    Mundo 01 y 02

--- mundo.sh
+++ mundo.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python $1 main.py
 
+if [[ "$1" ]] && [[ "$1" = "01" ]];
+then echo "Mundo 01" && cd $1 && python $2 main.py && cd ..;
+elif [[ "$1" ]] && [[ "$1" = "02" ]];
+then echo "Mundo 02" && cd $1 && python $2 main.py && cd ..;
+fi;
